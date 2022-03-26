d1e93b24b6d963cc4ef302834c11f4eddbf78c55
MohammadFaysal731 <www.faysal731@gmail.com>
2022-03-26 08:07:03 +0600

    add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { GiBilledCap } from 'react-icons/gi';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className='header'>
+            <GiBilledCap className='icon-left'></GiBilledCap>
+            <h1>Cap Collection</h1>
+            <GiBilledCap className='icon-right'></GiBilledCap>
+        </div>
+    );
+};
+
+export default Header; <h2>hello</h2>
